1f717954ac0eaa68e922bfeccd174e9ceba7f0f5
Pedro Carrillo <pf.cr@icloud.com>
2022-10-07 17:31:13 -0500

    Initial Commit

---
+++ LuloBank/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LuloBank
+//
+//  Created by Admin on 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LuloBank/LuloBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuloBankApp.swift
+//  LuloBank
+//
+//  Created by Admin on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LuloBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
